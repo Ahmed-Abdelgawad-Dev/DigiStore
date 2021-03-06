9837f79de73f88325f495b32da41a85b7084adc2
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-03-06 13:38:07 +0200

    showing the cart items when order is added
    showing photos of the products and list the orders dynamically

--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -14,7 +14,14 @@ def store(request):
 
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items': items}
     return render(request, 'store/cart.html', context)
 
 
