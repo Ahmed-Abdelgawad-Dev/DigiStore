9da43d9dc42e4046535822c8b43e7b738a74ed74
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-03-07 22:48:02 +0200

    cart is complete now and acting dynamically as well the checkout

--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -27,5 +27,13 @@ def cart(request):
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
