66a842ff839904079844c4fc27aa3d36900924ba
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-03-07 13:02:07 +0200

    set dynamic calculations for the cart items and the total

--- Ecommerce/store/models.py
+++ Ecommerce/store/models.py
@@ -41,6 +41,18 @@ class Order(models.Model):
     
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 class OrderItem(models.Model):
     product = models.ForeignKey(
@@ -49,6 +61,11 @@ class OrderItem(models.Model):
         Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=True, blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
+    
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShppingAdress(models.Model):
     customer = models.ForeignKey(
--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -21,7 +21,8 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
-    context = {'items': items}
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
