be891bca471d2876e3922acff3043b23edb45c58
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-03-18 13:47:47 +0200

    adding more code to handl cart of the anonymous user

--- Ecommerce/static/js/cart.js
+++ Ecommerce/static/js/cart.js
@@ -1,29 +1,45 @@
 var updateBtns = document.getElementsByClassName("update-cart");
 for (let i = 0; i < updateBtns.length; i++) {
-	updateBtns[i].addEventListener('click', function () {
+	updateBtns[i].addEventListener("click", function () {
 		let productId = this.dataset.product;
 		let action = this.dataset.action;
 		console.log("productID", productId, "action", action);
-		console.log('useris:', user);
-		if (user === 'AnonymousUser') {
+		console.log("useris:", user);
+		if (user === "AnonymousUser") {
 			// addCookieItem();
-			console.log('test')
+			console.log("test");
 		} else {
 			updateUserOrder(productId, action);
 		}
-
-
 	});
 }
 
-function addCookieItem(productId, action) {
-	console.log('Not logged in')
-}
+// function addCookieItem(productId, action) {
+// 	console.log("not logged in");
 
+// 	if(action == 'add') {
+// 		if(cart[prodoductId] == undefined) {
+// 			cart[productId] = {
+// 				'quantity': 1
+// 			};
+// 		} else {
+// 			cart[productId]['quantity'] += 1;
+// 		}
+// 	}
+// 	if(action == 'remove') {
+// 		cart[productId]["quantity"]  -= 1;
+// 		if(cart[productId]["quantity"] <= 0) {
+// 			console.log("remove item");
+// 			delete cart[productId];
+// 		}
+// 	}
+// 	console.log('cart: ', cart);
+// 	document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/";
+// }
 
 function updateUserOrder(productId, action) {
-	console.log('user is logged in sending data ...');
-	var url = '/update_item/';
+	console.log("user is logged in sending data ...");
+	var url = "/update_item/";
 	fetch(url, {
 			method: "POST",
 			headers: {
@@ -31,8 +47,8 @@ function updateUserOrder(productId, action) {
 				"X-CSRFToken": csrftoken,
 			},
 			body: JSON.stringify({
-				'productId': productId,
-				'action': action
+				productId: productId,
+				action: action,
 			}),
 		})
 		.then((response) => {
@@ -42,4 +58,31 @@ function updateUserOrder(productId, action) {
 			console.log("data: ", data);
 			location.reload();
 		});
+}
+
+
+
+function addCookieItem(productId, action) {
+  console.log("User is not authenticated");
+
+  if (action == "add") {
+    if (cart[productId] == undefined) {
+      cart[productId] = { quantity: 1 };
+    } else {
+      cart[productId]["quantity"] += 1;
+    }
+  }
+
+  if (action == "remove") {
+    cart[productId]["quantity"] -= 1;
+
+    if (cart[productId]["quantity"] <= 0) {
+      console.log("Item should be deleted");
+      delete cart[productId];
+    }
+  }
+  console.log("CART:", cart);
+  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
+
+  location.reload();
 }
--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -36,9 +36,24 @@ def cart(request):
         cartItems = order.get_cart_items
         
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {} 
+        print(cart)
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
+        for i in cart:
+            cartItems += cart[i]['quantity']
+            product = Product.objects.get(id=i)
+            total = (product.price * cart[i]['quantity'])
+            
+            order['get_cart_total'] += total
+            order['get_cart_total'] += cart[i]['quantity']
+            
+            item = {}
+            
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
